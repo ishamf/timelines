077aaa2c463254516263d38165e4fd40529e6b54
Isham Faizal <i@adimaja.com>
2017-02-16 23:26:08 +0700

    Removed brunch template remnants

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Counter from './Counter'
 
 export default class App extends React.Component {
   render () {
     return (
-      <div id='content'>
-        <Counter />
-      </div>
+      <div id='content' />
     )
   }
 }
